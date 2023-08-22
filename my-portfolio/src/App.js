9e00d265b86cea57e24f2eacd54d26ffc0290a9e
ZRQ-rikkie <Z986929216@gmail.com>
2023-08-22 21:07:41 +0100

    change

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import About from './componets/about/About.jsx';
+import Home from './componets/home/Home.jsx'
+import Portfolio from './componets/portfolio/Portfolio.jsx';
+import Resume from './componets/resume/Resume.jsx';
+import Services from './componets/services/Services.jsx';
+import Sidebar from './componets/sidebar/Sidebar.jsx';
+import Testmonials from './componets/testimonials/Testmonials';
+import Blog from './componets/blog/Blog';
+import Contact from './componets/contact/Contact';
+import Pricing from './componets/pricing/Pricing';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Sidebar/>
+    <main>
+      <Home/>
+      <About/>
+      <Services/>
+      <Resume/>
+      <Portfolio/>
+      <Pricing/>
+      <Testmonials/>``
+      <Blog/>
+      <Contact/>
+
+    </main>
+    </>
   );
 }
 
