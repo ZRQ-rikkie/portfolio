19f59f69553ee0ca90d8f9b8c5440b1a424fcf30
ZRQ-rikkie <Z986929216@gmail.com>
2023-08-29 22:34:17 +0100

    setup about page

--- my-portfolio/src/componets/About/About.jsx
+++ my-portfolio/src/componets/About/About.jsx
@@ -1,12 +1,56 @@
 import React from 'react'
 import './about.css'
+import Image from "../../assets/avatar-2.svg"
 
-const about = () => {
+const About = () => {
   return (
-    <div>
+    <section className="about container section" id='about'>
+      <h2 className="section__title">About Me</h2>
+      <div className="about__container grid">
+        <img src={Image} alt="" className="about__img" />
 
-    </div>
+        <div className="about__data grid">
+          <div className="about__info ">
+            <p className="about__description">I am Bolby Doe, web developer from London, United Kingdom. I have rich experience in web site design and building and customization, also I am good at WordPress.</p>
+            <a href="#home" className="btn">Download CV</a>
+          </div>
+
+          <div className="about__skills grid">
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">Development</h3>
+                <span className="skills__number development">90%</span>
+              </div>
+              <div className="skills__bar">
+                <span className="skills_percentage"></span>
+              </div>
+            </div>
+
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">Photography</h3>
+                <span className="skills__number">60%</span>
+              </div>
+              <div className="skills__bar">
+                <span className="skills_percentage"></span>
+              </div>
+            </div>
+
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">UI/UX design</h3>
+                <span className="skills__number">80%</span>
+              </div>
+              <div className="skills__bar">
+                <span className="skills_percentage ui__design"></span>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default about
+export default About
