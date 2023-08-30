6f542fae1ecfa6144c0f2f075de3f7ad5a3b8bae
ZRQ-rikkie <Z986929216@gmail.com>
2023-08-30 11:57:13 +0100

    Adding resume

--- src/componets/home/Home.jsx
+++ src/componets/home/Home.jsx
@@ -8,6 +8,7 @@ import Shapes from './Shapes'
 const Home = () => {
   return (
     <section className="home container" id="home">
+
       <div className="intro">
         <img src={Me} alt="" className="home__img" />
         <h1 className="home__name">Ruiqi Zhao</h1>
@@ -15,9 +16,11 @@ const Home = () => {
         <HeaderSocials/>
       <a href="#contact" className="btn" >Hire Me</a>
       <ScrollDown/>
+      <Shapes/>
+
+
 
       </div>
-      <Shapes/> 
 
     </section>
   )
--- src/componets/resume/Resume.jsx
+++ src/componets/resume/Resume.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './resume.css'
+import Data from './Data'
 
 const Resume = () => {
   return (
-    <div>
+    <section className="resume container section" id="resume">
+      <h2 className="section__tittle">Experience</h2>
 
-    </div>
+      <div className="resume__container grid">
+        <div className="timeline grid">
+          {Data.map(val,id)} => {
+
+          }
+
+        </div>
+      </div>
+    </section>
   )
 }
 
