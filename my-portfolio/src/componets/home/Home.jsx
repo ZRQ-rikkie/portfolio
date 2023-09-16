c9dcb20bc8dcd3ae43a5bb6589f2b805e4738648
ZRQ-rikkie <Z986929216@gmail.com>
2023-09-16 22:40:06 +0100

    Adding typing animations to home page

--- my-portfolio/src/componets/home/Home.jsx
+++ my-portfolio/src/componets/home/Home.jsx
@@ -1,30 +1,50 @@
-import React from 'react'
-import './home.css'
-import Me from "../../assets/me.png"
-import HeaderSocials from './HeaderSocials'
-import ScrollDown from './ScrollDown'
-import Shapes from './Shapes'
+import React, { useState, useEffect } from 'react';
+import './home.css';
+import Me from "../../assets/me.png";
+import HeaderSocials from './HeaderSocials';
+import ScrollDown from './ScrollDown';
 
 const Home = () => {
+  const [title, setTitle] = useState('');
+  const [h1Completed, setH1Completed] = useState(false); // 新的状态标记
+
+  const typeWriter = (text, setter, callback) => {
+    let index = 0;
+    let currentText = "";
+    const interval = setInterval(() => {
+      if (index < text.length) {
+        currentText += text.charAt(index);
+        setter(currentText);
+        index++;
+      } else {
+        clearInterval(interval);
+        if (callback) callback();
+      }
+    }, 70);
+  };
+
+  useEffect(() => {
+    const fullTitle = "Hi, I'm Ruiqi Zhao";
+
+    typeWriter(fullTitle, setTitle, () => {
+      setH1Completed(true); 
+    });
+  }, []);
+
   return (
     <section className="home container" id="home">
-
       <div className="intro">
         <img src={Me} alt="" className="home__img" />
-        {/* <h1 className="home__name">Ruiqi Zhao</h1> */}
-        {/* <h2 className="home__education">I'm a web developer</h2> */}
-        <HeaderSocials/>
-      <a href="#contact" className="btn" >Hire Me</a>
-
-      <ScrollDown/>
-      {/* <Shapes/> */}
-
-
-
+        <h1 className="home__name">{title}</h1>
+         <p className={`home__education ${h1Completed ? 'fade-in' : 'fade-out'}`}>
+        Coding enthusiast, problem-solver at heart.
+      </p>
+        <HeaderSocials />
+        <a href="#contact" className="btn">Hire Me</a>
+        <ScrollDown />
       </div>
-
     </section>
-  )
+  );
 }
 
-export default Home
+export default Home;
